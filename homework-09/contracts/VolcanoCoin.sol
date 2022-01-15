5c56a80ef45ceb1bc5d485ab726bc150a05af946
Miguel Gagliardo <migag9@gmail.com>
2022-01-15 19:36:09 +0000

    Feat: Homework 13 (#14)

--- homework-09/contracts/VolcanoCoin.sol
+++ homework-09/contracts/VolcanoCoin.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol"; 
-import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract VolcanoCoin is ERC20("Volcano Coin", "VLC"), Ownable {
 
